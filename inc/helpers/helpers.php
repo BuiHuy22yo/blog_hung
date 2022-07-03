14aa17f830f067b0a6c88928511cdb40d1a73f1a
Buihuy22yo <buihuy22yo@gmail.com>
2022-07-03 10:52:19 +0700

    update pagination

--- inc/helpers/helpers.php
+++ inc/helpers/helpers.php
@@ -442,15 +442,15 @@ if (!function_exists('ctwp_ajax_Load_comment')) {
 //
 
 if (!function_exists('ctwpGetPagination_html')) {
-    function ctwpGetPagination_html($limit = '', $current_page = '', $date_type = '')
+    function ctwpGetPagination_html($next_page = '')
     {
-        if (!$current_page) {
-            echo '';
-            exit();
-        }
-        $limit = !empty($limit) ? $limit : 10;
-        $current_page = !empty($current_page) ? $current_page : 1;
-//        $data = ctwpGetAllTopicByForum($limit, $current_page);
+        $html = '';
+//        if (!$current_page) {
+//            echo '';
+//            exit();
+//        }
+//        $posts_per_page = !empty($limit) ? $limit : 10;
+        $next_page = !empty($next_page) ? $next_page : 1;
         $data = ctwpGetAllTopicByForum();
         if (!$data) {
             echo '';
@@ -459,86 +459,59 @@ if (!function_exists('ctwpGetPagination_html')) {
         $posts = array_key_exists('data', $data) ? $data['data'] : [];
         $total = array_key_exists('total', $data) ? $data['total'] : '';
         $max_page = array_key_exists('max_page', $data) ? $data['max_page'] : '';
-        $current_page = array_key_exists('current_page', $data) ? $data['current_page'] : '';
+        $current_page = array_key_exists('current_page', $data) ? $data['current_page'] : 1;
         $posts_per_page = array_key_exists('posts_per_page', $data) ? $data['posts_per_page'] : '';
-        if (!$posts || !$total || !$max_page || $current_page || !$posts_per_page) {
+
+        if (!$posts || !$total || !$max_page || !$current_page || !$posts_per_page) {
             echo '';
             exit();
         }
         $disable_prev = $current_page == 1 ? 'disable' : '';
         $disable_next = $current_page == $max_page ? 'disable' : '';
-        ?>
-        <?php if ($total && $limit && $total > $limit) { ?>
-                <div class="pagination">
-                    <ul>
-                        <li class="page-item prev <?php echo $disable_prev ?>">
-                            <?php if ($current_page > 1) { ?>
-                            <span>
-                                <?php } ?>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29"
-                                     fill="none">
-                                    <circle cx="14.5" cy="14.5" r="14" transform="rotate(-180 14.5 14.5)"
-                                            stroke="black"/>
-                                    <path d="M12 22L19.0711 14.9289L12 7.85786" stroke="black" stroke-linecap="round"
-                                          stroke-linejoin="round"/>
-                                </svg>
-                                <?php if ($current_page > 1) { ?>
-                            </span>
-                        <?php } ?>
-                        </li>
-                        <?php
-                        $last_page = 0;
-                        $next_page = 0;
-                        for ($page = 1; $page <= $max_page; $page++) {
+        if ($total && $posts_per_page && $total > $posts_per_page) {
+            $prev = $current_page - 1 != 0 ? $current_page - 1 : '';
+            $next = $current_page + 1 != $max_page ? $current_page + 1 : '';
+            $html .= ' <div class="pagination d-flex justify-content-center my-4"><ul class="d-flex align-items-center">';
+            $html .= '<li class="page-item prev me-1 '.$disable_prev.'">';
+            $html .= '<span><svg class="rotate-180" xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29" fill="none"><circle cx="14.5" cy="14.5" r="14" transform="rotate(-180 14.5 14.5)" stroke="black"/><path d="M12 22L19.0711 14.9289L12 7.85786" stroke="black" stroke-linecap="round" stroke-linejoin="round"/></svg></span>';
+            $html .= '<input type="hidden" id="next_page" value="'.$prev.'"></li>';
+            $html .= '</li>';
+            $last_page_flag = 0;
+            $next_page_flag = 0;
+            for ($page = 1; $page <= $max_page; $page++) {
 
-                            if ($page != $max_page && $page > $current_page + 2) {
-                                $last_page = $page == $max_page - 1 ? 1 : 0;
-                                $page_disable = ' page-item-number-disable';
-                            } else if ($page != 1 && $page < $current_page - 2) {
-                                $next_page = $page == 2 ? 1 : 0;
-                                $page_disable = ' page-item-number-disable';
-                            } else {
-                                $last_page = 0;
-                                $next_page = 0;
-                                $page_disable = '';
-                            }
+                if ($page != $max_page && $page > $current_page + 2) {
+                    $last_page_flag = $page == $max_page - 1 ? 1 : 0;
+                    $page_disable = ' page-item-number-disable';
+                } else if ($page != 1 && $page < $current_page - 2) {
+                    $next_page_flag = $page == 2 ? 1 : 0;
+                    $page_disable = ' page-item-number-disable';
+                } else {
+                    $last_page_flag = 0;
+                    $next_page_flag = 0;
+                    $page_disable = '';
+                }
 
-                            $page_active = $current_page == $page ? ' page-item-active' : '';
-                            ?>
-                            <?php if ($next_page === 1) { ?>
-                                <li class="page-item-dots"><span class="page-numbers dots">…</span></li>
-                            <?php } ?>
-                            <?php if ($last_page === 1) { ?>
-                                <li class="page-item-dots"><span class="page-numbers dots ">…</span></li>
-                            <?php } ?>
-                            <li class="page-item page-item-number<?php echo $page_disable ?><?php echo $page_active ?>">
-                                <span><?php echo $page ?></span></li>
+                $page_active = $current_page == $page ? ' page-item-active' : '';
+                if ($next_page_flag === 1) {
+                    $html .= '<li class="page-item-dots"><span class="page-numbers dots">…</span></li>';
+                }
+                if ($last_page_flag === 1) {
+                    $html .= '<li class="page-item-dots"><span class="page-numbers dots ">…</span></li>';
+                }
+                $html .= '<li class="page-item page-item-number p-1 mx-1'.$page_disable.' '.$page_active .'"><span>'.$page .'</span><input type="hidden" id="next_page" value="'.$page .'"></li>';
 
-                        <?php } ?>
-                        <li class="page-item next <?php echo $disable_next ?>">
-                            <?php if ($current_page < $max_page) { ?>
-                            <span>
-                                <?php } ?>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29"
-                                     fill="none">
-                                    <circle cx="14.5" cy="14.5" r="14" transform="rotate(-180 14.5 14.5)"
-                                            stroke="black"/>
-                                    <path d="M12 22L19.0711 14.9289L12 7.85786" stroke="black" stroke-linecap="round"
-                                          stroke-linejoin="round"/>
-                                </svg>
-                                <?php if ($current_page < $max_page) { ?>
-                                    </span>
-                        <?php } ?>
-                        </li>
-                    </ul>
-                </div>
-                <input type="hidden" id="limit" value="<?php echo $limit ?>">
-                <input type="hidden" id="current_page" value="<?php echo $current_page ?>">
-                <input type="hidden" id="date_type" value="<?php echo $date_type ?>">
-                <?php
-            } ?>
-        <?php wp_reset_query();
-        die();
+            }
+            $html .= '<li class="page-item next ms-1 '.$disable_next.'">';
+            $html .= '<span><svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29" fill="none"><circle cx="14.5" cy="14.5" r="14" transform="rotate(-180 14.5 14.5)"stroke="black"/><path d="M12 22L19.0711 14.9289L12 7.85786" stroke="black" stroke-linecap="round"stroke-linejoin="round"/></svg></span>';
+            $html .= '<input type="hidden" id="next_page" value="'.$next.'"></li>';
+            $html .= '</li>';
+            $html .= '<input type="hidden" id="current_page" value="'.$current_page.'">';
+            $html .= '</ul></div>';
+        }
+
+        $html .= '</div>';
+        return $html;
     }
 }
 
@@ -552,12 +525,11 @@ if (!function_exists('ctwp_ajax_archive_pagination')) {
             }
             $limit = $_POST['limit'];
             $current_page = $_POST['current_page'];
-            $date_type = $_POST['date_type'];
-            if (!$date_type || !$current_page || !$limit) {
+            if (!$current_page || !$limit) {
                 echo '';
                 exit;
             }
-            ctwpGetArchivePagination_html($limit, $current_page, $date_type);
+            ctwpGetArchivePagination_html($limit, $current_page);
             die();
 
         } catch (Exception $e) {
--- template-parts/archives/list.php
+++ template-parts/archives/list.php
@@ -10,6 +10,7 @@ $data = ctwpGetAllTopicByForum();
 $topics = !empty($data) && array_key_exists('data', $data) ? $data['data'] : [];
 $max_page = !empty($data) && array_key_exists('max_page', $data) ? $data['max_page'] : 0;
 $current_page = !empty($data) && array_key_exists('current_page', $data) ? $data['current_page'] : 0;
+$posts_per_page = !empty($data) && array_key_exists('posts_per_page', $data) ? $data['posts_per_page'] : 10;
 $forums = ctwpGetAllForum(['title']);
 $idForums = ctwpGetForumNew();
 $idForums = array_shift($idForums);
@@ -30,56 +31,69 @@ $idForums = array_shift($idForums);
     </div>
 </div>
 <div class="col-9">
-    <div class="archive-post-list">
-        <?php if ($topics) { ?>
-            <article <?php post_class('post-forum-topic border border-color-white'); ?>>
-                <div class="col-inner inner-head border-b border-color-white py-2">
-                    <div class="inner-item row ">
-                        <div class="col-9 topic-title text-center">Topics</div>
-                        <div class="col-3 topic-reply text-center">Last reply</div>
-                    </div>
-                </div>
-                <?php foreach ($topics as $topic) { ?>
-                    <div class="col-inner inner-body border-b border-color-white py-2">
+    <div class="archive-post">
+        <div class="archive-post-inner archive-post-pagination">
+            <?php echo ctwpGetPagination_html() ?>
+        </div>
+        <div class="archive-post-inner archive-post-list">
+            <?php if ($topics) { ?>
+                <article <?php post_class('post-forum-topic border border-color-white'); ?>>
+                    <div class="col-inner inner-head border-b border-color-white py-2">
                         <div class="inner-item row ">
-                            <div class="col-9 topic d-flex">
-                                <div class="topic-image ctwp-mw-50 mx-2">
-                                    <img class="w-100" src="<?php echo ctwpGetAvatarUser('avatar', $topic->post_author)?>" alt="">
-                                </div>
-                                <div class="topic-info flex-grow-1">
-                                    <div class="topic-title">
-                                        <a class="link-title" href="<?php echo get_the_permalink($topic->ID) ?>">
-                                            <span class="text-title"><?php echo get_the_title($topic->ID) ?></span>
-                                        </a>
+                            <div class="col-9 topic-title text-center">Topics</div>
+                            <div class="col-3 topic-reply text-center">Last reply</div>
+                        </div>
+                    </div>
+                    <?php foreach ($topics as $topic) { ?>
+                        <div class="col-inner inner-body border-b border-color-white py-2">
+                            <div class="inner-item row ">
+                                <div class="col-9 topic d-flex">
+                                    <div class="topic-image ctwp-mw-50 mx-2">
+                                        <img class="w-100"
+                                             src="<?php echo ctwpGetAvatarUser('avatar', $topic->post_author) ?>"
+                                             alt="">
                                     </div>
-                                    <div class="topic-info-inner d-flex">
-                                        <div class="topic-author-name me-2"><span>Author: </span><?php echo get_the_author_meta('display_name', $topic->post_author);?></span></div>
-                                        <div class="topic-create-date"><span>Create at: </span><?php echo get_the_date('d/m/Y', $topic->ID) ?></div>
+                                    <div class="topic-info flex-grow-1">
+                                        <div class="topic-title">
+                                            <a class="link-title" href="<?php echo get_the_permalink($topic->ID) ?>">
+                                                <span class="text-title"><?php echo get_the_title($topic->ID) ?></span>
+                                            </a>
+                                        </div>
+                                        <div class="topic-info-inner d-flex">
+                                            <div class="topic-author-name me-2">
+                                                <span>Author: </span><?php echo get_the_author_meta('display_name', $topic->post_author); ?></span>
+                                            </div>
+                                            <div class="topic-create-date">
+                                                <span>Create at: </span><?php echo get_the_date('d/m/Y', $topic->ID) ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="cmt">
+                                        <i class="fa-regular fa-comment"></i>
+                                        <span class="count"><?php echo ctwpGetTotalComment($topic->ID) ?></span>
                                     </div>
                                 </div>
-                                <div class="cmt">
-                                    <i class="fa-regular fa-comment"></i>
-                                    <span class="count"><?php echo ctwpGetTotalComment($topic->ID)?></span>
-                                </div>
-                            </div>
-                            <div class="col-3 author-reply d-flex">
-                                <div class="author-image ctwp-mw-40">
-                                    <img class="w-100" src="<?php echo ctwpGetAvatarUser('avatar', $topic->post_author)?>" alt="">
-                                </div>
-                                <div class="author-info px-2">
-                                    <div class="author-name">mcnb02</div>
-                                    <div class="author-date">01/01/2020</div>
+                                <div class="col-3 author-reply d-flex">
+                                    <div class="author-image ctwp-mw-40">
+                                        <img class="w-100"
+                                             src="<?php echo ctwpGetAvatarUser('avatar', $topic->post_author) ?>"
+                                             alt="">
+                                    </div>
+                                    <div class="author-info px-2">
+                                        <div class="author-name">mcnb02</div>
+                                        <div class="author-date">01/01/2020</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                <?php } ?>
-            </article>
-            <?php
-        } ?>
+                    <?php } ?>
+                </article>
+                <?php
+            } ?>
+        </div>
+        <div class="archive-post-inner archive-post-pagination">
+            <?php echo ctwpGetPagination_html() ?>
+        </div>
     </div>
-<!--    --><?php //echo ctwpGetPagination_html()?>
-
-
 </div>
 
